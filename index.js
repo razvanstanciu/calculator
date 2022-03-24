f27ed2533ba503ddc9a005f0ce66cf8bfd9a3174
Stanciu Razvan <razvanstanciuba@gmail.com>
2022-03-24 16:26:49 +0200

    adding clear and delete button

---
+++ index.js
@@ -0,0 +1,9 @@
+const num = document.querySelectorAll('.num');
+const display = document.querySelector('.display');
+let counter = 0;
+
+function clickMeAction() {  
+    if(activType === num.values){
+        
+    }
+}
